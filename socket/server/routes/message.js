8bab1cdaa5cb6e229eaca028918e84fc851cb007
NachiketJoy <64661004+NachiketJoy@users.noreply.github.com>
2024-12-18 21:44:01 +1100

    Delete socket directory

--- socket/server/routes/message.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const taskController = require('../controllers/messageController');
-
-router.get('/api/messages', taskController.fetchMessage);
-router.post('/api/message', taskController.saveMessage);
-
-module.exports = router;
